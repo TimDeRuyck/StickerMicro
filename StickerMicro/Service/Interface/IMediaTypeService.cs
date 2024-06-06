eee836de41af61d130e767e0bccbd2391359ac55
Tim De Ruyck <113930948+TimDeRuyck@users.noreply.github.com>
2024-06-06 14:13:13 +0200

    Add interifce IMediaTypeService

---
+++ StickerMicro/Service/Interface/IMediaTypeService.cs
@@ -0,0 +1,16 @@
+﻿using StickerMicro.core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickerMicro.Service.Interface
+{
+    public interface IMediaTypeService
+    {
+        Task<MediaType> AddMediaTypeAsync(MediaType mediaType);
+        Task<MediaType> UpdateMediaTypeAsync(MediaType mediaType);
+        Task DeleteMediaTypeAsync(int mediaTypeId);
+    }
+}
