4b45aa1cce88382b26b1ba5c6120d016e2240167
Tim De Ruyck <113930948+TimDeRuyck@users.noreply.github.com>
2024-06-06 13:09:50 +0200

    Add LabelingSystemContext

---
+++ StickerMicro.core/Data/LabelingSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickerMicro.core.Data
+{
+    public class LabelingSystemContext : DbContext
+    {
+        public LabelingSystemContext(DbContextOptions<LabelingSystemContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Entities.MediaType> MediaTypes { get; set; }
+        public DbSet<Entities.PrintLabel> PrintLabels { get; set; }
+    }
+}
